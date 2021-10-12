eec30f0009817aac33d1b212536ba3c693ddb9d1
sh3lbsd <86627394+sh3lbsd@users.noreply.github.com>
2021-10-11 21:45:54 -0500

    ginished bonus on servlets-exercise

--- src/main/java/CountServlet.java
+++ src/main/java/CountServlet.java
@@ -9,13 +9,19 @@ public class CountServlet extends HttpServlet {
     int count = 0;
 
 
+
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
+        //   BONUS: Allow the user to pass a parameter in the query string to reset the counter.
+        String passedCount = request.getParameter("count");
+        // if there is no name in the url query, it will return null, so then we set null equal to a string
+        if (passedCount != null) count = Integer.valueOf(passedCount);
         //every time user requests the page we increment a count plus one
-        count ++;
+        else count ++;
         PrintWriter out = response.getWriter();
-        out.println("<h1> Page Viewed: " + count + " times.</h1>");
+        out.println("<h1> Page Viewed: " + count + " time(s).</h1>");
     }
 
 }
